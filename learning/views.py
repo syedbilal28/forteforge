1d6e57df732e51d17977d66fa60588364046f332
Abdul Wassay <wassaysardar35@gmail.com>
2021-07-01 19:11:30 +0500

    Add student signup view backend

--- learning/views.py
+++ learning/views.py
@@ -1,24 +1,31 @@
 from learning.models import Student
 from django.shortcuts import redirect, render, HttpResponse
-from . import forms
+# from . import forms
+from .forms import ProfileForm,UserForm
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 @csrf_exempt
 def login(request):
     if request.method == 'POST':
-        data = request.POST
         return HttpResponse("<h1>Logged In</h1>")
     else:
-        form = forms.UserForm()
+        form = UserForm()
         return render(request, 'learning/login.html', {'form':form})
 
 @csrf_exempt
 def signup(request):
     if request.method == 'POST':
-        data = request.POST
-        return HttpResponse("<h1>Signed Up</h1>")
+        userform = UserForm(request.POST)
+        profileform = ProfileForm(request.POST, request.FILES)
+        if userform.is_valid() and profileform.is_valid():
+            user = userform.save()
+            profile = profileform.save(user)
+            return HttpResponse("<h1>Signed Up</h1>")
+        #for dev
+        else:
+            raise Exception (f"Profile form not valid {profileform}")
     else:
-        userform = forms.UserForm()
-        profileform = forms.ProfileForm()
-        return render(request, 'learning/signup.html', {'userform':userform, 'profileform':profileform})
+        userform = UserForm()
+        profileform = ProfileForm()
+    return render(request, 'learning/signup.html', {'userform':userform, 'profileform':profileform})
