6d93596ccf083e0afb4cc38926117a7be5b4e76e
syedbilal28 <syed.bilal.sba@gmail.com>
2021-10-14 14:29:19 +0500

    student signup form

--- learning/urls.py
+++ learning/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("",views.index,name="index"),
-    path('signup/', views.signup, name='signup'),
-    path("choice/",views.signup_choice,name='SignupChoice'),
+    path('signup/student/', views.signup, name='signup'),
+    path("signup/choice/",views.signup_choice,name='SignupChoice'),
     path('login/', views.login, name='login'),
     path("careers/",views.Careers,name="Careers"),
     path('signup/enterprise/',views.signup_enterprise,name="Signupenterprise"),
